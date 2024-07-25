ce74dcb3e2a9b7204afe4268ca6dc5cbbfd5b85d
naresh-khatri <naresh.khatri2345@gmail.com>
2024-07-25 21:51:40 +0530

    feat: :sparkles: add header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import Link from "next/link";
+import styles from "./style.module.scss";
+import { opacity, background } from "./anim";
+import Nav from "./nav";
+
+interface HeaderProps {
+  loader?: boolean;
+}
+
+const Header = ({ loader }: HeaderProps) => {
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  return (
+    <motion.header
+      className={styles.header}
+      style={{
+        background: isActive ? "hsl(var(--background) / .8)" : "transparent",
+      }}
+      initial={{
+        y: -80,
+      }}
+      animate={{
+        y: 0,
+      }}
+      transition={{
+        delay: loader ? 3.5 : 0, // 3.5 for loading, .5 can be added for delay
+        duration: 0.8,
+      }}
+    >
+      <div className={styles.bar}>
+        <Link href="/" className="flex items-center justify-center">
+          <span className="text-md font-semibold transition-transform hover:translate-x-1 hover:translate-y-1">
+          Naresh Khatri
+          </span>
+        </Link>
+        <div onClick={() => setIsActive(!isActive)} className={styles.el}>
+          <div className={styles.label}>
+            <motion.p
+              variants={opacity}
+              animate={!isActive ? "open" : "closed"}
+            >
+              Menu
+            </motion.p>
+            <motion.p variants={opacity} animate={isActive ? "open" : "closed"}>
+              Close
+            </motion.p>
+          </div>
+          <div
+            className={`${styles.burger} ${
+              isActive ? styles.burgerActive : ""
+            }`}
+          ></div>
+        </div>
+      </div>
+      <motion.div
+        variants={background}
+        initial="initial"
+        animate={isActive ? "open" : "closed"}
+        className={styles.background}
+      ></motion.div>
+      <AnimatePresence mode="wait">
+        {isActive && <Nav setIsActive={setIsActive} />}
+      </AnimatePresence>
+    </motion.header>
+  );
+};
+
+export default Header;
---
+++ src/components/header/nav/index.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import styles from './style.module.scss';
+import { height } from '../anim';
+import Body from './body/body';
+import Footer from './footer/footer';
+import Image from './image/image';
+import ModeToggle from '@/components/mode-toggle';
+
+import { links } from '@/components/header/config';
+
+interface IndexProps {
+  setIsActive: (isActive: boolean) => void;
+}
+
+interface SelectedLinkState {
+  isActive: boolean;
+  index: number;
+}
+
+const Index: React.FC<IndexProps> = ({ setIsActive }) => {
+  const [selectedLink, setSelectedLink] = useState<SelectedLinkState>({
+    isActive: false,
+    index: 0
+  });
+
+  return (
+    <motion.div
+      variants={height}
+      initial="initial"
+      animate="enter"
+      exit="exit"
+      className={styles.nav}
+    >
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <Body
+            links={links}
+            selectedLink={selectedLink}
+            setSelectedLink={setSelectedLink}
+            setIsActive={setIsActive}
+          />
+          {/* <Footer /> */}
+        </div>
+        <Image
+          src={links[selectedLink.index].thumbnail}
+          isActive={selectedLink.isActive}
+        />
+        <div className="absolute bottom-0 right-0 mb-auto">
+          <ModeToggle />
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Index;
