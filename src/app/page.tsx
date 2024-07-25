b3ca553dba5e2dff0c5c87927a7bf330cedd2f3b
naresh-khatri <naresh.khatri2345@gmail.com>
2024-07-25 21:54:10 +0530

    feat: :sparkles: add smooth scroll and preloader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,46 +2,53 @@
 
 import React from "react";
 import Link from "next/link";
-import Button from "@/components/ui/Button";
+// import Button from "@/components/ui/Button";
 
 import { FaFile, FaGithub, FaLinkedin } from "react-icons/fa6";
+import SmoothScroll from "@/components/smooth-scroll";
+import Preloader from "@/components/preloader";
+import { Button } from "@/components/ui/button";
 
 function MainPage() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black overflow-x-hidden">
-      {/* <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-301/0 via-zinc-300/50 to-zinc-300/0" /> */}
-      <h1 className="z-10 font-thin text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-4xl md:text-7xl whitespace-nowrap bg-clip-text ">
-        Hi, I am <br className="md:hidden"/> Naresh Khatri 
-      </h1>
-      {/* <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" /> */}
-      <h1 className="z-10 mt-4 font-thin text-xl text-transparent duration-1000 bg-zinc-400 cursor-default text-edge-outline animate-title font-display sm:text-xl md:text-xl whitespace-nowrap bg-clip-text ">
-        A Full Stack Web Developer
-      </h1>
+    <SmoothScroll>
+      <Preloader />
 
-      <div className="my-16 text-center animate-fade-in gap-3 flex justify-around">
-        <Link
-          href={
-            "https://drive.google.com/file/d/12cV9lMDqK5VxnMu2NxpdQt4WIOB-spaZ/view"
-          }
-        >
-          <Button onClick={() => console.log("hello")}>
-            <div className="flex gap-2">
-              <FaFile size={24} /> Resume
-            </div>
-          </Button>
-        </Link>
-        <Link href={"https://github.com/Naresh-Khatri"}>
-          <Button>
-            <FaGithub size={24} />
-          </Button>
-        </Link>
-        <Link href={"https://www.linkedin.com/in/naresh-khatri/"}>
-          <Button>
-            <FaLinkedin size={24} />
-          </Button>
-        </Link>
-      </div>
-      {/* <div className="my-16 text-center animate-fade-in">
+      <main style={{ height: "100dvh" }}>
+        <div className="flex flex-col items-center justify-center w-full h-full overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black overflow-x-hidden">
+          {/* <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-301/0 via-zinc-300/50 to-zinc-300/0" /> */}
+          <h1 className="font-thin text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-4xl md:text-7xl whitespace-nowrap bg-clip-text ">
+            Hi, I am <br className="md:hidden" /> Naresh Khatri
+          </h1>
+          {/* <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" /> */}
+          <h1 className="mt-4 font-thin text-xl text-transparent duration-1000 bg-zinc-400 cursor-default text-edge-outline animate-title font-display sm:text-xl md:text-xl whitespace-nowrap bg-clip-text ">
+            A Full Stack Web Developer
+          </h1>
+
+          <div className="my-16 text-center animate-fade-in gap-3 flex justify-around">
+            <Link
+              href={
+                "https://drive.google.com/file/d/12cV9lMDqK5VxnMu2NxpdQt4WIOB-spaZ/view"
+              }
+            >
+              <Button onClick={() => console.log("hello")}>
+                <div className="flex gap-2">
+                  <FaFile size={24} /> Resume
+                </div>
+              </Button>
+            </Link>
+            <Link href={"https://github.com/Naresh-Khatri"}>
+              <Button>
+                <FaGithub size={24} />
+              </Button>
+            </Link>
+            <Link href={"https://www.linkedin.com/in/naresh-khatri/"}>
+              <Button>
+                <FaLinkedin size={24} />
+              </Button>
+            </Link>
+          </div>
+          {/* <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
           I'm building{" "}
           <Link
@@ -54,7 +61,9 @@ function MainPage() {
           to solve API authentication and authorization for developers.
         </h2>
       </div> */}
-    </div>
+        </div>
+      </main>
+    </SmoothScroll>
   );
 }
 
