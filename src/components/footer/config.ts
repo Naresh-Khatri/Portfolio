56a3b60627f7857b6f64eca39f1bdfa29f9880d0
naresh-khatri <naresh.khatri2345@gmail.com>
2024-08-14 21:42:24 +0530

    feat: :sparkles: add footer

---
+++ src/components/footer/config.ts
@@ -0,0 +1,12 @@
+const footer: { title: string; href: string }[] = [
+  {
+    title: "Blog",
+    href: "https://blog.example.com",
+  },
+  {
+    title: "Newsletter",
+    href: "https://newsletter.example.com",
+  },
+];
+
+export { footer };
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'next/link';
+import { footer } from './config';
+
+
+function Footer() {
+  return (
+    <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t border-border px-4 py-6 sm:flex-row md:px-6">
+      <p className="text-xs text-gray-500 dark:text-gray-400">
+        © 2024-future Naresh Khatri. All rights reserved.
+      </p>
+      <nav className="flex gap-4 sm:ml-auto sm:gap-6">
+        {footer.map((link, index) => {
+          const { title, href } = link;
+
+          return (
+            <Link
+              className="text-xs underline-offset-4 hover:underline"
+              href={href}
+              key={`l_${index}`}
+            >
+              {title}
+            </Link>
+          );
+        })}
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
