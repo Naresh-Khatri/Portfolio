ff9610def616ffb0899bdcd94808bf753ceee502
naresh-khatri <naresh.khatri2345@gmail.com>
2024-08-14 21:46:59 +0530

    feat: :sparkles: add animated background with projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,57 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-// import Button from "@/components/ui/Button";
-
-import { FaFile, FaGithub, FaLinkedin } from "react-icons/fa6";
 import SmoothScroll from "@/components/smooth-scroll";
 import Preloader from "@/components/preloader";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import About from "@/app/about/page";
+import Projects from "@/components/sections/projects";
+import Hero from "@/components/sections/hero";
+import AnimatedBackground from "@/components/animated-background";
+import Footer from "@/components/footer/footer";
 
 function MainPage() {
   return (
-    <SmoothScroll>
-      <Preloader />
-
-      <main style={{ height: "100dvh" }}>
-        <div className="flex flex-col items-center justify-center w-full h-full overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black overflow-x-hidden">
-          {/* <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-301/0 via-zinc-300/50 to-zinc-300/0" /> */}
-          <h1 className="font-thin text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-4xl md:text-7xl whitespace-nowrap bg-clip-text ">
-            Hi, I am <br className="md:hidden" /> Naresh Khatri
-          </h1>
-          {/* <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" /> */}
-          <h1 className="mt-4 font-thin text-xl text-transparent duration-1000 bg-zinc-400 cursor-default text-edge-outline animate-title font-display sm:text-xl md:text-xl whitespace-nowrap bg-clip-text ">
-            A Full Stack Web Developer
-          </h1>
-
-          <div className="my-16 text-center animate-fade-in gap-3 flex justify-around">
-            <Link
-              href={
-                "https://drive.google.com/file/d/12cV9lMDqK5VxnMu2NxpdQt4WIOB-spaZ/view"
-              }
-            >
-              <Button onClick={() => console.log("hello")}>
-                <div className="flex gap-2">
-                  <FaFile size={24} /> Resume
-                </div>
-              </Button>
-            </Link>
-            <Link href={"https://github.com/Naresh-Khatri"}>
-              <Button>
-                <FaGithub size={24} />
-              </Button>
-            </Link>
-            <Link href={"https://www.linkedin.com/in/naresh-khatri/"}>
-              <Button>
-                <FaLinkedin size={24} />
-              </Button>
-            </Link>
+    <>
+      <SmoothScroll>
+        <Preloader />
+        <main
+          // style={{ height: "100dvh", width: "100dvw" }}
+          className={cn("bg-slate-100 dark:bg-transparent")}
+        >
+          <div className="top-0 z-[0] fixed w-screen h-screen">
+            <AnimatedBackground />
           </div>
-          {/* <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link>{" "}
-          to solve API authentication and authorization for developers.
-        </h2>
-      </div> */}
-        </div>
-      </main>
-    </SmoothScroll>
+          <section id="section-0" className={cn("relative w-screen h-screen")}>
+            <Hero />
+          </section>
+
+          {/* <section id="section-1" className="z-[100] bg-blue-400 h-[200vh]">
+            <h1 className="top-[50px] sticky text-8xl text-center"> hello</h1>
+          </section> */}
+          <section id="skills" className="bg-iblue-400 w-screen h-[200dvh]">
+            <div className="top-[70px] sticky">
+              <h2 className="bg-clip-text bg-gradient-to-b from-white/80 to-white/20 bg-opacity-50 text-4xl text-center text-transparent md:text-7xl">
+                SKILLS
+              </h2>
+              <p className="mx-auto mt-4 line-clamp-4 max-w-3xl font-normal text-base text-center text-neutral-300">
+                (hint: press a key)
+              </p>
+            </div>
+            {/* hello */}
+          </section>
+          <section id="projects" className="min-h-screen z-[9999]">
+            <h2 className="bg-clip-text bg-gradient-to-b from-white/80 to-white/20 bg-opacity-50 text-4xl text-center text-transparent md:text-7xl">
+              Projects
+            </h2>
+            <Projects />
+          </section>
+          <Footer />
+        </main>
+      </SmoothScroll>
+    </>
   );
 }
 
