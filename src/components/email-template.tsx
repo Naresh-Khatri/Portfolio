295d326ca238ae1be9f42bccf0c630ccc38ce050
naresh-khatri <naresh.khatri2345@gmail.com>
2024-08-20 00:46:48 +0530

    feat: :fire: create a basic email template for resend

---
+++ src/components/email-template.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+interface EmailTemplateProps {
+  fullName: string;
+  email: string;
+  message: string;
+}
+
+export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
+  fullName,
+  email,
+  message,
+}) => (
+  <div>
+    <h1>Welcome, {fullName}!</h1>
+    <div className="text-red-500">{email} sent you a message</div>
+    <blockquote>{message}</blockquote>
+  </div>
+);
