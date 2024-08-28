069abe87cdb8b9abf3a3ce47a0213e321affc39b
naresh-khatri <naresh.khatri2345@gmail.com>
2024-08-28 16:54:13 +0530

    feat: :zap: design new cursor

--- src/components/ui/ElasticCursor.tsx
+++ src/components/ui/ElasticCursor.tsx
@@ -1,93 +1,187 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useLayoutEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
+import { cn } from "@/lib/utils";
+import { useMouse } from "@/hooks/use-mouse";
 
-function ElasticCursor() {
-  const circleRef = useRef<HTMLDivElement>(null);
-
-  // Create objects to track mouse position and custom cursor position
-  const [mouse, setMouse] = useState({ x: 0, y: 0 }); // Track current mouse position
-  const [previousMouse, setPreviousMouse] = useState({ x: 0, y: 0 }); // Store the previous mouse position
-  const [circle, setCirle] = useState({ x: 0, y: 0 }); // Track the circle position
-
-  // Initialize variables to track scaling and rotation
-  let currentScale = 0; // Track current scale value
-  let currentAngle = 0; // Track current angle value
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.addEventListener("mousemove", updateMousePos);
-      tick();
+// Gsap Ticker Function
+function useTicker(callback: any, paused?: boolean) {
+  useLayoutEffect(() => {
+    if (!paused && callback) {
+      gsap.ticker.add(callback);
     }
     return () => {
-      if (typeof window !== "undefined") {
-        window.removeEventListener("mousemove", updateMousePos);
+      gsap.ticker.remove(callback);
+    };
+  }, [callback, paused]);
+}
+
+const EMPTY = {} as {
+  x: Function;
+  y: Function;
+  r?: Function;
+  width?: Function;
+  rt?: Function;
+  sx?: Function;
+  sy?: Function;
+};
+function useInstance(value = {}) {
+  const ref = useRef(EMPTY);
+  if (ref.current === EMPTY) {
+    ref.current = typeof value === "function" ? value() : value;
+  }
+  return ref.current;
+}
+
+// Function for Mouse Move Scale Change
+function getScale(diffX: number, diffY: number) {
+  const distance = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
+  return Math.min(distance / 735, 0.35);
+}
+
+// Function For Mouse Movement Angle in Degrees
+function getAngle(diffX: number, diffY: number) {
+  return (Math.atan2(diffY, diffX) * 180) / Math.PI;
+}
+
+function getRekt(el: HTMLElement) {
+  if (el.classList.contains("cursor-can-hover"))
+    return el.getBoundingClientRect();
+  else if (el.parentElement?.classList.contains("cursor-can-hover"))
+    return el.parentElement.getBoundingClientRect();
+  else if (
+    el.parentElement?.parentElement?.classList.contains("cursor-can-hover")
+  )
+    return el.parentElement.parentElement.getBoundingClientRect();
+  return null;
+}
+
+const CURSOR_DIAMETER = 50;
+
+function ElasticCursor() {
+  // React Refs for Jelly Blob and Text
+  const jellyRef = useRef<HTMLDivElement>(null);
+  const [isHovering, setIsHovering] = useState(false);
+  const { x, y } = useMouse();
+
+  // Save pos and velocity Objects
+  const pos = useInstance(() => ({ x: 0, y: 0 }));
+  const vel = useInstance(() => ({ x: 0, y: 0 }));
+  const set = useInstance();
+
+  // Set GSAP quick setter Values on useLayoutEffect Update
+  useLayoutEffect(() => {
+    set.x = gsap.quickSetter(jellyRef.current, "x", "px");
+    set.y = gsap.quickSetter(jellyRef.current, "y", "px");
+    set.r = gsap.quickSetter(jellyRef.current, "rotate", "deg");
+    set.sx = gsap.quickSetter(jellyRef.current, "scaleX");
+    set.sy = gsap.quickSetter(jellyRef.current, "scaleY");
+    set.width = gsap.quickSetter(jellyRef.current, "width", "px");
+  }, []);
+
+  // Start Animation loop
+  const loop = useCallback(() => {
+    if (!set.width || !set.sx || !set.sy || !set.r) return;
+    // Calculate angle and scale based on velocity
+    var rotation = getAngle(+vel.x, +vel.y); // Mouse Move Angle
+    var scale = getScale(+vel.x, +vel.y); // Blob Squeeze Amount
+
+    // Set GSAP quick setter Values on Loop Function
+    if (!isHovering) {
+      set.x(pos.x);
+      set.y(pos.y);
+      set.width(50 + scale * 250);
+      set.r(rotation);
+      set.sx(1 + scale);
+      set.sy(1 - scale * 1.3);
+    } else {
+      set.r(0);
+    }
+  }, [isHovering]);
+
+  // Run on Mouse Move
+  useLayoutEffect(() => {
+    // Caluclate Everything Function
+    const setFromEvent = (e: MouseEvent) => {
+      if (!jellyRef.current) return;
+      const el = e.target as HTMLElement;
+      const hoverElemRect = getRekt(el);
+      if (hoverElemRect) {
+        const rect = el.getBoundingClientRect();
+        setIsHovering(true);
+        gsap.to(jellyRef.current, {
+          rotate: 0,
+          duration: 0,
+        });
+        gsap.to(jellyRef.current, {
+          width: el.offsetWidth + 20,
+          height: el.offsetHeight + 20,
+          x: rect.left + rect.width / 2,
+          y: rect.top + rect.height / 2,
+          borderRadius: 20,
+          duration: 2,
+          ease: "elastic.out(1, 0.3)",
+        });
+
+        // return;
+      } else {
+        gsap.to(jellyRef.current, {
+          borderRadius: 50,
+          width: CURSOR_DIAMETER,
+          height: CURSOR_DIAMETER,
+        });
+        setIsHovering(false);
       }
+      // Mouse X and Y
+      const x = e.clientX;
+      const y = e.clientY;
+
+      // Animate Position and calculate Velocity with GSAP
+      gsap.to(pos, {
+        x: x,
+        y: y,
+        duration: 1.5,
+        ease: "elastic.out(1, 0.5)",
+        onUpdate: () => {
+          // @ts-ignore
+          vel.x = (x - pos.x) * 1.2;
+          // @ts-ignore
+          vel.y = (y - pos.y) * 1.2;
+        },
+      });
+
+      loop();
+    };
+
+    window.addEventListener("mousemove", setFromEvent);
+    return () => {
+      window.removeEventListener("mousemove", setFromEvent);
     };
   }, []);
-  const updateMousePos = (e: MouseEvent) => {
-    //  setMouse({ x: e.x, y: e.y });
-    mouse.x = e.x;
-    mouse.y = e.y;
-  };
 
-  const speed = 0.055;
+  useTicker(loop);
 
-  const tick = () => {
-    // MOVE
-    // Calculate circle movement based on mouse position and smoothing
-    // setCirle((p) => ({
-    //   x: (mouse.x - circle.x) * speed,
-    //   y: (mouse.y - circle.y) * speed,
-    // }));
-    circle.x += (mouse.x - circle.x) * speed;
-    circle.y += (mouse.y - circle.y) * speed;
-    // Create a transformation string for cursor translation
-    const translateTransform = `translate(${circle.x}px, ${circle.y}px)`;
-
-    // SQUEEZE
-    // 1. Calculate the change in mouse position (deltaMouse)
-    const deltaMouseX = mouse.x - previousMouse.x;
-    const deltaMouseY = mouse.y - previousMouse.y;
-    // Update previous mouse position for the next frame
-    // setPreviousMouse({ x: mouse.x, y: mouse.y });
-    previousMouse.x = mouse.x;
-    previousMouse.y = mouse.y;
-    // 2. Calculate mouse velocity using Pythagorean theorem and adjust speed
-    const mouseVelocity = Math.min(
-      Math.sqrt(deltaMouseX ** 2 + deltaMouseY ** 2) * 4,
-      250
-    );
-    // 3. Convert mouse velocity to a value in the range [0, 0.5]
-    const scaleValue = (mouseVelocity / 150) * 0.5;
-    // 4. Smoothly update the current scale
-    currentScale += (scaleValue - currentScale) * speed;
-    // 5. Create a transformation string for scaling
-    const scaleTransform = `scale(${1 + currentScale}, ${1 - currentScale})`;
-
-    // ROTATE
-    // 1. Calculate the angle using the atan2 function
-    const angle = (Math.atan2(deltaMouseY, deltaMouseX) * 180) / Math.PI;
-    // 2. Check for a threshold to reduce shakiness at low mouse velocity
-    if (mouseVelocity > 20) {
-      currentAngle = angle;
-    }
-    // 3. Create a transformation string for rotation
-    const rotateTransform = `rotate(${currentAngle}deg)`;
-
-    // Apply all transformations to the circle element in a specific order: translate -> rotate -> scale
-    if (!circleRef.current) return;
-    circleRef.current.style.transform = `${translateTransform} ${rotateTransform} ${scaleTransform}`;
-
-    // Request the next frame to continue the animation
-    window.requestAnimationFrame(tick);
-  };
-
+  // Return UI
   return (
-    <div
-      ref={circleRef}
-      style={{ zIndex: 100, backdropFilter: "invert(100%)" }}
-      className="hidden sm:block fixed w-[40px] h-[40px] border-[1px] border-solid border-white rounded-full cursor-none top-[-20px] left-[-20px] pointer-events-none"
-    ></div>
+    <>
+      <div
+        ref={jellyRef}
+        id={"jelly-id"}
+        className={cn(
+          `w-[${CURSOR_DIAMETER}px] h-[${CURSOR_DIAMETER}px] border-2 border-black dark:border-white`,
+          "jelly-blob fixed left-0 top-0 rounded-full will-change-transform z-[999]",
+          "pointer-events-none translate-x-[-50%] translate-y-[-50%]"
+        )}
+        style={{
+          zIndex: 100,
+          backdropFilter: isHovering ? "" : "invert(100%)",
+        }}
+      ></div>
+      <div
+        className="bg-black w-3 h-3 rounded-full fixed translate-x-[-50%] translate-y-[-50%] pointer-events-none"
+        style={{ top: y, left: x }}
+      ></div>
+    </>
   );
 }
 
