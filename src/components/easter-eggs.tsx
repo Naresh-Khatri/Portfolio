3c9fd8ca5120eb7db854b9d725e48231ac1392e6
naresh-khatri <naresh.khatri2345@gmail.com>
2024-08-30 12:21:59 +0530

    feat: :sparkles: use get syntax to define name function

--- src/components/easter-eggs.tsx
+++ src/components/easter-eggs.tsx
@@ -18,36 +18,43 @@ const EasterEggs = () => {
     //   "color: #00FF00; font-size: 16px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px;"
     // );
     if (typeof console !== "undefined") {
+      console.clear();
       console.log(
         "%cWhoa, look at you! 🕵️‍♂️\n" +
           "You seem to have discovered the secret console! 🔍\n" +
           "Want to see some magic? ✨\n" +
-          "Just call %cNaresh%c fn and hit enter! 🎩🐇",
+          "Just type %cmy first name%c and hit enter! 🎩🐇",
         //   "Just press the %c'n'%c key and watch the magic happen! 🪄",
         "color: #FFD700; font-size: 16px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px; margin-top:20px",
-        "color: #00FF00; font-size: 16px; font-family: monospace; padding: 10px;",
+        "color: #00FF00; font-size: 16px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px; margin-top:20px",
         "color: #FFD700; font-size: 16px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px;"
       );
 
-      // @ts-ignore
-      window.Naresh = () => {
-        console.log(
-          "%c✨ Abra Kadabra! ✨\n\n" +
-            "You just summoned the magic of Naresh! 🧙‍♂️\n" +
-            "What??? youre not impressed? Fine, but remember: With great power comes great responsibility! 💻⚡",
+      ["naresh", "Naresh", "NARESH"].forEach((name) => {
+        // @ts-ignore
+        if (Object.hasOwn(window, name)) return;
+        Object.defineProperty(window, name, {
+          get() {
+            console.log(
+              "%c✨ Abra Kadabra! ✨\n\n" +
+                "You just summoned the magic of Naresh! 🧙‍♂️\n" +
+                "What??? youre not impressed? Fine, but remember: With great power comes great responsibility! 💻⚡",
 
-          "color: #FF4500; font-size: 18px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px; margin-top:10px"
-        );
+              "color: #FF4500; font-size: 18px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px; margin-top:10px"
+            );
 
-        const timer = setTimeout(() => {
-          console.log(
-            "%cPssttt! 🐾\n\n" +
-              "Do you like cats?? 😺 If yes, then press 'n' on viewport and see what happens! 🐱✨",
-            "color: #FF69B4; font-size: 16px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px;"
-          );
-          clearTimeout(timer);
-        }, 7000);
-      };
+            const timer = setTimeout(() => {
+              console.log(
+                "%cPssttt! 🤫\n\n" +
+                  "Do you like cats?? 😺 If yes, then press 'n' on viewport and see what happens! 🐱✨",
+                "color: #FF69B4; font-size: 16px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px;"
+              );
+              clearTimeout(timer);
+            }, 7000);
+            return "";
+          },
+        });
+      });
     }
   }, [isDevToolsOpen]);
 
