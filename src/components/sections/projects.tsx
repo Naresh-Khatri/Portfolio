3ca030320b7755802cd51708d6694013dad854f3
naresh-khatri <naresh.khatri2345@gmail.com>
2024-08-30 13:15:01 +0530

    feat: open link in new tab

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -59,7 +59,7 @@ const Modall = ({ project }: { project: Project }) => {
             <button className="px-2 py-1 bg-gray-200 text-black dark:bg-black dark:border-black dark:text-white border border-gray-300 rounded-md text-sm w-28">
               Cancel
             </button>
-            <Link href={project.live}>
+            <Link href={project.live} target="_blank">
               <button className="bg-black text-white dark:bg-white dark:text-black text-sm px-2 py-1 rounded-md border border-black w-28">
                 Visit
               </button>
