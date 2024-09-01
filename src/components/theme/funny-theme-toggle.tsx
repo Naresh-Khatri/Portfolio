b412efe00b1970ca90b3393501b2e438087864de
naresh-khatri <naresh.khatri2345@gmail.com>
2024-09-01 10:10:04 +0530

    feat: :bug: add pointer event none on icons

--- src/components/theme/funny-theme-toggle.tsx
+++ src/components/theme/funny-theme-toggle.tsx
@@ -43,8 +43,8 @@ export default function FunnyThemeToggle({
           className={cn("border-none bg-transparent", className)}
           onClick={goDark}
         >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-500 dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all duration-500 dark:rotate-0 dark:scale-100" />
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-500 dark:-rotate-90 dark:scale-0 pointer-events-none" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all duration-500 dark:rotate-0 dark:scale-100 pointer-events-none" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       ) : (
