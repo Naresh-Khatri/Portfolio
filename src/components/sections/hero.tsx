313e15ba34b355e5bc917d26917933e3e016c421
naresh-khatri <naresh.khatri2345@gmail.com>
2024-09-01 10:10:56 +0530

    feat: :sparkles: add blurIn animation

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -8,8 +8,12 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { usePreloader } from "../preloader";
+import { BlurIn } from "../reveal-animations";
 
 const Hero = () => {
+  const { isLoading } = usePreloader();
+
   return (
     <div className="grid md:grid-cols-2">
       <div
@@ -20,44 +24,54 @@ const Hero = () => {
           "pt-28 sm:pt-0 sm:pb-32 md:p-24 lg:p-40 xl:p-48"
         )}
       >
-        <div className="">
-          <p
-            className={cn(
-              "md:self-start mt-4 font-thin text-md text-slate-500 dark:text-zinc-400 ml-2",
-              "cursor-default font-display sm:text-xl md:text-xl whitespace-nowrap bg-clip-text "
-            )}
-          >
-            Hi, I am
-            <br className="md:hidden" />
-          </p>
-          <Tooltip delayDuration={300}>
-            <TooltipTrigger asChild>
-              <h1
+        {!isLoading && (
+          <div className="">
+            <BlurIn delay={0.7}>
+              <p
                 className={cn(
-                  "font-thin text-6xl text-transparent text-slate-800 ml-1",
-                  "cursor-default text-edge-outline font-display sm:text-7xl md:text-9xl "
+                  "md:self-start mt-4 font-thin text-md text-slate-500 dark:text-zinc-400 ml-3",
+                  "cursor-default font-display sm:text-xl md:text-xl whitespace-nowrap bg-clip-text "
                 )}
               >
-                Naresh <br className="md:block hiidden" /> Khatri
-              </h1>
-            </TooltipTrigger>
-            <TooltipContent
-              side="top"
-              className="dark:bg-white dark:text-black"
-            >
-              theres something waiting for you in devtools
-            </TooltipContent>
-          </Tooltip>
-          {/* <div className="md:block hidden bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 w-screen h-px animate-fade-right animate-glow" /> */}
-          <p
-            className={cn(
-              "md:self-start md:mt-4 font-thin text-md text-slate-500 dark:text-zinc-400 ml-2",
-              "cursor-default font-display sm:text-xl md:text-xl whitespace-nowrap bg-clip-text "
-            )}
-          >
-            A Full Stack Web Developer
-          </p>
-        </div>
+                Hi, I am
+                <br className="md:hidden" />
+              </p>
+            </BlurIn>
+            <BlurIn delay={1}>
+              <Tooltip delayDuration={300}>
+                <TooltipTrigger asChild>
+                  <h1
+                    className={cn(
+                      "font-thin text-6xl text-transparent text-slate-800 ml-1 text-left",
+                      "cursor-default text-edge-outline font-display sm:text-7xl md:text-9xl "
+                    )}
+                  >
+                    Naresh
+                    <br className="md:block hiidden" />
+                    Khatri
+                  </h1>
+                </TooltipTrigger>
+                <TooltipContent
+                  side="top"
+                  className="dark:bg-white dark:text-black"
+                >
+                  theres something waiting for you in devtools
+                </TooltipContent>
+              </Tooltip>
+            </BlurIn>
+            {/* <div className="md:block hidden bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 w-screen h-px animate-fade-right animate-glow" /> */}
+            <BlurIn delay={1.2}>
+              <p
+                className={cn(
+                  "md:self-start md:mt-4 font-thin text-md text-slate-500 dark:text-zinc-400 ml-3",
+                  "cursor-default font-display sm:text-xl md:text-xl whitespace-nowrap bg-clip-text "
+                )}
+              >
+                A Full Stack Web Developer
+              </p>
+            </BlurIn>
+          </div>
+        )}
 
         <div className="mt-8 md:ml-1 flex flex-col gap-3">
           <Link
