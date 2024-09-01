1af7e6d22ff0f16162dbb176506a655e54f886b6
naresh-khatri <naresh.khatri2345@gmail.com>
2024-09-02 00:30:37 +0530

    feat: :art: return margin top

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -62,7 +62,7 @@ const ContactForm = () => {
     setLoading(false);
   };
   return (
-    <form className="min-w-7xl mx-auto mt-16" onSubmit={handleSubmit}>
+    <form className="min-w-7xl mx-auto sm:mt-4" onSubmit={handleSubmit}>
       <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
         <LabelInputContainer>
           <Label htmlFor="fullname">Full name</Label>
