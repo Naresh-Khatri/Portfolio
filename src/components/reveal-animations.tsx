cc5b9e3a86d5340e2c946f3bf980243c997fc843
naresh-khatri <naresh.khatri2345@gmail.com>
2024-09-02 10:53:36 +0530

    feat: :sparkles: add box reveal animation

--- src/components/reveal-animations.tsx
+++ src/components/reveal-animations.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useAnimation, useInView } from "framer-motion";
 
 import { cn } from "@/lib/utils";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 
 interface BlurIntProps {
   children: ReactNode;
@@ -41,5 +41,77 @@ export const BlurIn = ({
     >
       {children}
     </motion.h1>
+  );
+};
+
+interface BoxRevealProps {
+  children: JSX.Element;
+  width?: "fit-content" | "100%";
+  boxColor?: string;
+  duration?: number;
+  delay?: number;
+  once?: boolean;
+}
+export const BoxReveal = ({
+  children,
+  width = "fit-content",
+  boxColor,
+  duration,
+  delay,
+  once = true,
+}: BoxRevealProps) => {
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once });
+
+  useEffect(() => {
+    if (isInView) {
+      slideControls.start("visible");
+      mainControls.start("visible");
+    } else {
+      slideControls.start("hidden");
+      mainControls.start("hidden");
+    }
+  }, [isInView, mainControls, slideControls]);
+
+  return (
+    <div ref={ref} style={{ position: "relative", width, overflow: "hidden" }}>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: duration ? duration : 0.5, delay }}
+      >
+        {children}
+      </motion.div>
+
+      <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={slideControls}
+        transition={{
+          duration: duration ? duration : 0.5,
+          ease: "easeIn",
+          delay,
+        }}
+        style={{
+          position: "absolute",
+          top: 4,
+          bottom: 4,
+          left: 0,
+          right: 0,
+          zIndex: 20,
+          background: boxColor ? boxColor : "#ffffff00",
+        }}
+      />
+    </div>
   );
 };
