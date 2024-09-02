f37b922f43c8d1bdbace2eae502f5f32ae912c91
naresh-khatri <naresh.khatri2345@gmail.com>
2024-09-02 11:33:13 +0530

    feat: :sparkles: add social buttons in footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import Link from "next/link";
 import { footer } from "./config";
 import { Button } from "../ui/button";
+import SocialMediaButtons from "../social/social-media-icons";
 
 function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t border-border px-4 py-6 sm:flex-row md:px-6">
+    <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t border-border px-4 py-6 sm:flex-row md:px-6 sm:justify-between">
       <p className="text-xs text-gray-500 dark:text-gray-400">
         © {year} Naresh Khatri. All rights reserved.
       </p>
-      <nav className="flex gap-4 sm:ml-auto sm:gap-6 z-10">
+      <SocialMediaButtons />
+      <nav className="flex gap-4 sm:gap-6 z-10">
         {footer.map((link, index) => {
           const { title, href } = link;
 
