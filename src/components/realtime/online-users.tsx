7c8a79727664d8edc865754d4bf0701340f1bff3
naresh-khatri <naresh.khatri2345@gmail.com>
2024-09-03 20:28:29 +0530

    feat: :lipstick: add animations

--- src/components/realtime/online-users.tsx
+++ src/components/realtime/online-users.tsx
@@ -1,12 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { motion } from "framer-motion";
 
 import { SocketContext } from "@/contexts/socketio";
 
@@ -14,17 +13,52 @@ const OnlineUsers = () => {
   const { remoteCursors } = useContext(SocketContext);
   const cursors = Array.from(remoteCursors.values());
   if (cursors.length === 0) return null;
+  const listParent = useRef<HTMLUListElement>(null);
+
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.04,
+      },
+    },
+  };
+  const item = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+  };
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button variant="ghost">
-          <span> {cursors.length} online</span>
+        <Button variant="ghost" className="py-0 m-0">
+          <div className="flex items-center gap-2">
+            <div className="w-2 h-2 animate-pulse rounded-full bg-green-400"></div>
+            {cursors.length} online
+          </div>
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-52">
-        <div className="grid gap-4">
-          {cursors.map((cursor) => (
-            <div key={cursor.id} className="flex items-center justify-between">
+      <PopoverContent className="w-80">
+        <div className="space-y-2 mb-8">
+          <h4 className="text-xl">Users</h4>
+          <p className="text-sm text-muted-foreground">
+            There {cursors.length === 1 ? "is" : "are"} {cursors.length} user
+            {cursors.length === 1 ? "" : "s"} online here!
+          </p>
+        </div>
+        <motion.ul
+          className="grid gap-4"
+          ref={listParent}
+          variants={container}
+          initial="hidden"
+          animate="show"
+        >
+          {cursors.map((cursor, i) => (
+            <motion.li
+              key={cursor.id}
+              className="flex items-center justify-between"
+              variants={item}
+            >
               <div className="flex items-center gap-2">
                 <div
                   className="w-4 h-4 rounded-full"
@@ -32,9 +66,9 @@ const OnlineUsers = () => {
                 ></div>
                 <span className="text-sm">{cursor.name}</span>
               </div>
-            </div>
+            </motion.li>
           ))}
-        </div>
+        </motion.ul>
       </PopoverContent>
     </Popover>
   );
